050c10cfdeed1df49a783aba0ad9177f7f580ef7
Geoff Bentley <geoff@philandteds.com>
2012-05-30 16:05:07 +1200

    added admin top tab called 'linguist'

---
+++ settings/menu.ini.append.php
@@ -0,0 +1,22 @@
+<?php /* 
+
+[TopAdminMenu]
+# This list contains menuitems of the top menu in admin interface
+Tabs[]=linguist
+
+[Topmenu_linguist]
+URL[]
+URL[default]=addicted/dictionaries
+NavigationPartIdentifier=ezcontentnavigationpart
+Name=Linguist
+Enabled[]
+Enabled[default]=true
+Enabled[browse]=false
+Enabled[edit]=false
+Shown[]
+Shown[default]=true
+Shown[navigation]=true
+Shown[browse]=false
+PolicyList[]=addicted/dictionaries
+
+*/ ?>
